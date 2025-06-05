fced0e8228f82148a14fca0eedc926d63ffb5f86
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2025-06-05 21:14:03 +0530

    create parent & child div

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world! from React"
-);
+/**
+ * <div id="parent">
+ * <div id="child">
+ *  <h1>I am an h1 tag</h1>
+ *  <h2>I am an h2 tag</h2>
+ * </div>
+ * </div>
+ */
 
-console.log(heading) // Object or react element
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag")
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag")
+  ])
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world! from React"
+// );
+
+console.log(parent); // Object or react element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
