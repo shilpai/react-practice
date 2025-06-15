e0aeddb3c4dcbe5b8faf1b9d0466559e5a384567
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2025-06-16 00:13:08 +0530

    first jsx code

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am shilpa sonule..."),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
+const heading = React.createElement(
+  "h2",
+  { id: "heading" },
+  "react using core react"
+);
+console.log(heading);
+
+//JSX => is a HTML like syntax or XML like syntax
+//JSX => (transpiled before it reaches the jS) => PARCEL => BABEL (transpile)
+const jsxHeading = <h1>Hello I am from jsx using heading</h1>;
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
