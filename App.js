8769839c83f0475bbd4b1cd88b630d3a2c85877e
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2025-07-03 23:18:41 +0530

    added body & restaurantcard component

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * #Header
+ * - Logo
+ * - Nav items
+ *
+ * #Body
+ * - Search
+ * - RestaurantContainer
+ *   - Restaurant Card
+ *
+ * #Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Content
+ */
+
+const styleCrad = {
+  backgroundColor: "#f0f0f0"
+};
+
 const Header = () => {
   return (
     <div className="header">
@@ -22,10 +43,34 @@ const Header = () => {
   );
 };
 
+const ReastaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCrad}>
+      <img
+      className="res-logo"
+      src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/7/5/edec8cd2-313c-4fce-8b7f-b5873ac590ac_40bd387c-9352-4729-882b-aedf2a945d13.JPG" />
+
+      <h3>Shilpa Foods</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-card">
+        <ReastaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const Applayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
