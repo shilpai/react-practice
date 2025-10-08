a0fbc0941fb1e6df19a32cd821cfa816b5152da1
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2025-10-08 21:13:01 +0530

    shimmer UI & login logic done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
-
+import { useState } from "react";
+  
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState("Login");
+  console.log("Header rendered");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +19,7 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li className="login" onClick={() => { isLoggedIn === "Login" ? setIsLoggedIn("Logout") : setIsLoggedIn("Login") }}>{isLoggedIn}</li>
         </ul>
       </div>
     </div>
